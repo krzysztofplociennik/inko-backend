5d0b53fa0e3695c70fad71bbc6a11e8e107aab6a
kplociennik <krzysztofplociennik6@gmail.com>
2024-07-19 21:28:51 +0200

    Minor changes.

--- model/src/main/java/com/plociennik/model/repository/ArticleRepository.java
+++ model/src/main/java/com/plociennik/model/repository/ArticleRepository.java
@@ -3,5 +3,7 @@ package com.plociennik.model.repository;
 import com.plociennik.model.ArticleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<ArticleEntity, String> {
+import java.util.UUID;
+
+public interface ArticleRepository extends JpaRepository<ArticleEntity, UUID> {
 }
