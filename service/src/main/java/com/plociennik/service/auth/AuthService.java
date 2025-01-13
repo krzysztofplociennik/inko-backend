d068c3839987047e81d31fd5b8fa7528c5ba0b4b
kplociennik <krzysztofplociennik6@gmail.com>
2025-01-13 19:26:10 +0100

    Login part 5

--- service/src/main/java/com/plociennik/service/auth/AuthService.java
+++ service/src/main/java/com/plociennik/service/auth/AuthService.java
@@ -4,9 +4,9 @@ import com.plociennik.model.UserEntity;
 import com.plociennik.model.repository.user.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +17,7 @@ public class AuthService {
     private final JwtTokenUtil jwtTokenUtil;
     private final UserRepository userRepository;
     private final PasswordEncoderConfig passwordEncoderConfig;
+    private final PasswordEncoder passwordEncoder;
 
     private static final String LOGIN_SUCCESS_MESSAGE = "Logged in";
     private static final String LOGIN_FAILURE_MESSAGE = "Invalid credentials";
@@ -24,28 +25,28 @@ public class AuthService {
     public ResponseEntity<AuthResponse> auth(LoginRequest loginRequest) {
         UserEntity user = userRepository.findByUsername(loginRequest.username())
                 .orElseThrow(() -> {
-                    logFailure(loginRequest.username());
+                    logFailure(loginRequest.username(), "180501132025");
                     return new UsernameNotFoundException(LOGIN_FAILURE_MESSAGE);
                 });
 
-        if (!passwordEncoderConfig.passwordEncoder().matches(loginRequest.password(), user.getPassword())) {
-            logFailure(loginRequest.username());
-            AuthResponse authResponse = new AuthResponse(null, LOGIN_FAILURE_MESSAGE);
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(authResponse);
+        boolean doesPasswordMatch = passwordEncoder.matches(loginRequest.password(), user.getPassword());
+        if (doesPasswordMatch) {
+            String token = jwtTokenUtil.generateToken(user);
+            logSuccess(loginRequest.username());
+            AuthResponse authResponse = new AuthResponse(token, LOGIN_SUCCESS_MESSAGE);
+            return ResponseEntity.ok(authResponse);
+        } else {
+            logFailure(loginRequest.username(), "192401132025");
+            throw new UsernameNotFoundException(LOGIN_FAILURE_MESSAGE);
         }
-
-        String token = jwtTokenUtil.generateToken(user);
-        logSuccess(loginRequest.username());
-        AuthResponse authResponse = new AuthResponse(token, LOGIN_SUCCESS_MESSAGE);
-        return ResponseEntity.ok(authResponse);
     }
 
     private void logSuccess(String username) {
         log.info("User successfully logged in, username: {}, (EID: {})", username, "081115122024");
     }
 
-    private void logFailure(String username) {
-        log.info("User failed to log in, username: {}, (EID: {})", username, "081415122024");
+    private void logFailure(String username, String eid) {
+        log.info("User failed to log in, username: {}, (EID: {})", username, eid);
     }
 }
 
