31e147acd95594659de067d7ef5b5bb5bb2c469a
kplociennik <krzysztofplociennik6@gmail.com>
2025-02-11 21:43:16 +0100

    Adding testing configuration to build.gradle

---
+++ service/src/test/groovy/com/plociennik/service/backup/ExportServiceTest.groovy
@@ -0,0 +1,10 @@
+package com.plociennik.service.backup
+
+import spock.lang.Specification
+
+class ExportServiceTest extends Specification {
+    def "SanitizeContent"() {
+        expect:
+        1 == 1 
+    }
+}
