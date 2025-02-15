08e3d45ae49ae7701187395746b030f994b3d2b7
kplociennik <krzysztofplociennik6@gmail.com>
2025-02-15 18:22:38 +0100

    Added silent error catching.

--- web/src/main/java/com/plociennik/web/config/CredentialsConfig.java
+++ web/src/main/java/com/plociennik/web/config/CredentialsConfig.java
@@ -7,10 +7,14 @@ public class CredentialsConfig {
     private CredentialsConfig() {}
 
     public static void load() {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        try {
+            Dotenv dotenv = Dotenv.load();
+            System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+            System.setProperty("DB_USER", dotenv.get("DB_USER"));
+            System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+            System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        } catch (Exception e) {
+            // silent catch
+        }
     }
 }
