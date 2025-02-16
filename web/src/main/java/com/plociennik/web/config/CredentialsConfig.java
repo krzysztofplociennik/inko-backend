f870fdd266f4ac717fd1d8f819a8583c3b6c4af1
kplociennik <krzysztofplociennik6@gmail.com>
2025-02-16 08:54:53 +0100

    Changed the database url.

--- web/src/main/java/com/plociennik/web/config/CredentialsConfig.java
+++ web/src/main/java/com/plociennik/web/config/CredentialsConfig.java
@@ -9,7 +9,7 @@ public class CredentialsConfig {
     public static void load() {
         try {
             Dotenv dotenv = Dotenv.load();
-            System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+            System.setProperty("CUSTOM_DATABASE_URL", dotenv.get("CUSTOM_DATABASE_URL"));
             System.setProperty("DB_USER", dotenv.get("DB_USER"));
             System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
             System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
