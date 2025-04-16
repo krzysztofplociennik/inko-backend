68f1a0a25aff1199cf20ca87aa59e10b091c154b
kplociennik <krzysztofplociennik6@gmail.com>
2025-04-16 12:05:24 +0200

    Deleted the cron/keep-alive job.

--- web/src/main/java/com/plociennik/web/Application.java
+++ web/src/main/java/com/plociennik/web/Application.java
@@ -4,7 +4,6 @@ import com.plociennik.web.config.CredentialsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(
@@ -15,7 +14,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 "com.plociennik.web",
         }
 )
-@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         CredentialsConfig.load();
