68027587fd2251c421d4ef4702a88667a2a2841e
krzysztofplociennik <krzysztofplociennik6@gmail.com>
2025-05-16 13:41:24 +0200

    Deleted @RequestParam and added @PathVariable to the reset method.

--- web/src/main/java/com/plociennik/web/api/admin/DemoResetController.java
+++ web/src/main/java/com/plociennik/web/api/admin/DemoResetController.java
@@ -3,10 +3,7 @@ package com.plociennik.web.api.admin;
 import com.plociennik.service.admin.DemoResetService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/demoReset")
@@ -15,8 +12,8 @@ public class DemoResetController {
 
     private final DemoResetService service;
 
-    @PostMapping(value = "/reset")
-    public ResponseEntity<String> reset(@RequestParam String adminToken) {
+    @PostMapping(value = "/reset/{adminToken}")
+    public ResponseEntity<String> reset(@PathVariable String adminToken) {
         service.reset(adminToken);
         return ResponseEntity.ok("Reset done.");
     }
